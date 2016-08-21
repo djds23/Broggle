f5bb2fd65b284c8f57eb900393e71b735806ab3d
Dean Silfen <dean.silfen@gmail.com>
2016-08-21 11:52:31 -0400

    Initial Commit

---
+++ Broggle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Broggle
+//
+//  Created by Dean Silfen on 8/21/16.
+//  Copyright © 2016 Dean Silfen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Broggle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Broggle
+//
+//  Created by Dean Silfen on 8/21/16.
+//  Copyright © 2016 Dean Silfen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
